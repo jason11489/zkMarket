03093f95eca0f3ad35baaa0ef4eb955d2d0a3a5c
jason11489 <donny11489@gmail.com>
2023-12-18 14:43:03 +0900

    setting

---
+++ zk-Market/zkMarket/src/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button, View } from 'react-native';
+
+
+const Layout = () => {
+    const next = () => {
+    console.log("button");
+}
+
+    return (
+        <View>
+            <Button title="next page"
+                color="#2c2c2c"
+                onPress={next} />
+        </View>
+    );
+};
+
+export default Layout;
