1cfb6b55bc345024d15c08fccc2ea61e6d235bad
jason11489 <donny11489@gmail.com>
2024-01-08 11:06:18 +0900

    finish search tab

--- zk-Market/zkMarket/src/CSS/Search_sytle.js
+++ zk-Market/zkMarket/src/CSS/Search_sytle.js
@@ -3,20 +3,21 @@ import { StyleSheet } from "react-native";
 export const Search_stytle = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:'white'
+        backgroundColor: 'white'
     },
     Scroll_style: {
         backgroundColor: 'white'
     },
     first_line: {
-        width: '100%',
-        height: '100%',
+        // width: '100%',
+        // height: '100%',
+        top : 10,
         position: 'relative',
         flexDirection: 'row',
-        justifyContent: 'flex-start',
-        alignItems: 'flex-start',
-        display: 'inline-flex',
-        left : 28
+        // justifyContent: 'flex-start',
+        // alignItems: 'flex-start',
+        // display: 'inline-flex',
+        left: 28
     },
     zkMarket_text: {
         width: 93,
@@ -30,7 +31,59 @@ export const Search_stytle = StyleSheet.create({
     shopping_bag: {
         width: 24,
         height: 24,
-        left : 219
+        left: 219
+    },
+    Search_book: {
+        top:30,
+        // width: '100%',
+        // height: '100%',
+        flexDirection: 'row',
+        // justifyContent: 'flex-start',
+        // alignItems: 'flex-start',
+        // display: 'inline-flex',
+        left: 9
+    },
+    Seacrh_background: {
+        width: 352,
+        height: 44,
+        left: 11,
+        backgroundColor: '#F8FAFF',
+        borderRadius: 22,
+        // overflow: 'hidden',
+        // justifyContent: 'flex-start',
+        // alignItems: 'center',
+        // display: 'inline-flex'
+    },
+    Search_icon: {
+        width: 24,
+        height: 24,
+        left: -320,
+        top: 10
+    },
+    Search_bar: {
+        backgroundColor: 'transparent',
+        left: -300,
+        top: 10,
+        width: 260,
+        height: 24,
+        fontSize: 14,
+        fontFamily: 'NanumSquareOTF_ac',
+        fontWeight: '700'
+    },
+    last_line: {
+        // width: '100%',
+        // height: '100%',
+        top:40,
+        position: 'relative',
+        flex:1
+    },
+    img: {
+        left : -10,
+        width: 410,
+        height: 590,
+        top:-10,
+        position: 'relative',
+        flex:1
     }
 
 })
--- zk-Market/zkMarket/src/Screens/Search.js
+++ zk-Market/zkMarket/src/Screens/Search.js
@@ -1,20 +1,46 @@
-import React from "react";
-import { Image, SafeAreaView, ScrollView, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+    Image,
+    SafeAreaView,
+    ScrollView,
+    Text,
+    TextInput,
+    View
+} from "react-native";
 
 import { Search_stytle } from "../CSS/Search_sytle";
 
 function Search() {
+
+    const [text, settext] = useState('hello');
+
     return (
         <SafeAreaView style={Search_stytle.container}>
             <ScrollView style={Search_stytle.Scroll_style}>
-                <View
-                    style={Search_stytle.first_line}>
+                <View style={Search_stytle.first_line}>
                     <Text style={Search_stytle.zkMarket_text}>
                         zkMarket
                     </Text>
-                    <Image style={Search_stytle.shopping_bag} source={require('../image/shopping_bag_gray.png')} />
+                    <Image
+                        style={Search_stytle.shopping_bag}
+                        source={require('../image/shopping_bag_gray.png')}/>
                 </View>
-
+                <View style={Search_stytle.Search_book}>
+                    <View style={Search_stytle.Seacrh_background}/>
+                    <Image
+                        style={Search_stytle.Search_icon}
+                        source={require('../image/search_gray.png')}/>
+                    <TextInput
+                        style={Search_stytle.Search_bar}
+                        placeholder="Search for what you want"
+                        placeholderTextColor='#909398'
+                        onChangeText={(text) => {
+                            settext(text)
+                        }}/>
+                </View>
+                <View style={Search_stytle.last_line}>
+                    <Image style={Search_stytle.img} source={require('../image/search_img.png')}/>
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
